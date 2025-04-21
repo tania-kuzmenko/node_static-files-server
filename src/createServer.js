e8de6255330689a95d85b6271a73c7c0e089d3de
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2025-04-21 04:11:10 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,56 @@
+/* eslint-disable max-len */
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer(async (req, res) => {
+    const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+
+    const fileName =
+      !normalizedUrl.pathname.slice(5) ||
+      normalizedUrl.pathname.slice(5) === '/'
+        ? 'index.html'
+        : normalizedUrl.pathname.sle(6);
+
+    if (fileName.includes('//')) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+
+      res.end('Dont use // in the path');
+
+      return;
+    }
+
+    const publicDir = path.resolve('public');
+    const filePath = path.resolve(publicDir, fileName);
+
+    if (!filePath.startsWith(publicDir)) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Access denied');
+
+      return;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (!err) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+
+        res.end(
+          'Hint: to load files, use path starting with "/file/", e.g. /file/index.html',
+        );
+      }
+    });
+  });
 }
 
 module.exports = {
